8a559be80c9c808890a55ba21c7d5bb373e1e633
epedrosa2905 <epedrosa@dxainvest.com>
2024-04-04 12:03:39 -0300

    instalando o axios + react-query

---
+++ src/api/https.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+const http = axios.create({
+  headers: { 'Content-Type': 'application/json' },
+  baseURL: process.env.EXPO_PUBLIC_API_BASE_URL,
+});
+
+http.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  async (error: AxiosError) => {
+    let errStatus = null;
+
+    if (error.response) {
+      errStatus = error.response.status;
+    }
+
+    // locais em que pode dar 401 mas o tratamento desse erro é feito diretamente no componente
+    if (errStatus) {
+      if (errStatus === 401) {
+      }
+      // QUANDO OCORRER ERRO DEFAULT DO SISTEMA
+      else if (errStatus === 500) {
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
